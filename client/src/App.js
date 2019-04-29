cf9da1519f16f69123df97860eb3cc966ba85a87
aditya745 <thakuraditya72@yahoo.in>
2019-04-29 16:25:38 +0300

    component names changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Scoreboard from "./components/scoreboard";
+import Scoreboard from "./components/Scoreboard";
 class App extends Component {
   render() {
     return (
